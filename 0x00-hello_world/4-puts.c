716eb5e72bcb3f2c36ae7d8f1d8c8601fdb77224
CharlesKufa <charleskufah@gmail.com>
2023-02-21 15:47:16 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - Printing a sentence line
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
